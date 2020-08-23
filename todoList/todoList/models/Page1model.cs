266863f54f0143c324c184a0263d8834f51ba6c3
boss <bpyr.wk@gmail.com>
2020-08-23 21:05:29 +0700

    data binding #1

---
+++ todoList/todoList/models/Page1model.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using todoList.views;
+
+namespace todoList.models
+{
+     class Page1model 
+    {
+        public ObservableCollection<Person> people { get; set; }
+        public Page1model() 
+        {
+            people = new ObservableCollection<Person>
+            {
+                
+                new Person("parinya","thingtavee","pathumthani"),
+                new Person("Arenakorn","Suk","pathumthani"),
+            };
+        }
+
+     
+    }
+    
+
+}
---
+++ todoList/todoList/views/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todoList.views
+{
+    class Person
+    {
+        public string firstName {get;set;}
+        public string lastName { get; set; }
+        public string city { get; set; }
+        public Person(string firstName, string lastName, string city)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.city = city;
+        }
+
+        public string fullName
+        {
+            get { return string.Format("{0} {1}", firstName, lastName); }
+        }
+    }
+}
